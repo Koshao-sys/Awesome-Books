c082ecb36b918f53163fbcb90a6615346c409960
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-02-14 19:51:19 +0300

    Refactor and use JS classes

--- app.js
+++ app.js
@@ -1,56 +1,73 @@
-let books = [];
+/* eslint-disable */
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+/* eslint-enable */
+
+class BookCollection {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(title, author) {
+    const newBook = new Book(title, author);
+    this.books.push(newBook);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
+
+  removeBook(index) {
+    /* eslint-disable */
+    this.books = this.books.filter((book, ind) => ind != index);
+    /* eslint-enable */
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
+
+  displayBooks() {
+    const bookListContainer = document.querySelector('.books-col');
+    bookListContainer.innerHTML = '';
+    for (let k = 0; k < this.books.length; k += 1) {
+      const bookItem = document.createElement('div');
+      bookItem.setAttribute('class', 'book-item');
+      bookItem.innerHTML = `
+        <span class="tle-author-holder">
+          <p class="book-title">${this.books[k].title}</p>
+          <span>by</span>
+          <p class="book-author">${this.books[k].author}</p>
+        </span>
+        <button class="remove-button${k}">Remove</button>
+      `;
+      bookListContainer.appendChild(bookItem);
+    }
+    this.addRemoveListeners();
+  }
+
+  addRemoveListeners() {
+    const removeBtn = document.querySelectorAll('.book-item button');
+    for (let i = 0; i < removeBtn.length; i += 1) {
+      removeBtn[i].addEventListener('click', () => {
+        const btnClass = removeBtn[i].className;
+        const index = btnClass.replace('remove-button', '');
+        this.removeBook(index);
+      });
+    }
+  }
+}
+
 const addBtn = document.getElementById('add-books');
-
-function displayBooks() {
-  const bookListContainer = document.querySelector('.books-col');
-  bookListContainer.innerHTML = '';
-  for (let k = 0; k < books.length; k += 1) {
-    const bookItem = document.createElement('div');
-    bookItem.setAttribute('class', 'book-item');
-    bookItem.innerHTML = `
-      <p class="book-title">${books[k].title}</p>
-      <p class="book-author">${books[k].author}</p>
-      <button class="remove-button${k}">Remove</button>
-    `;
-    bookListContainer.appendChild(bookItem);
-  }
-  /* eslint-disable */
-  addRemoveListeners();
-  /* eslint-enable */
-}
-
-function removeBook(index) {
-  /* eslint-disable */
-  books = books.filter((book, ind) => ind != index);
-  /* eslint-enable */
-  localStorage.setItem('books', JSON.stringify(books));
-  displayBooks();
-}
-
-function addRemoveListeners() {
-  const removeBtn = document.querySelectorAll('.book-item button');
-  for (let i = 0; i < removeBtn.length; i += 1) {
-    removeBtn[i].addEventListener('click', () => {
-      const btnClass = removeBtn[i].className;
-      const index = btnClass.replace('remove-button', '');
-      removeBook(index);
-    });
-  }
-}
-
-function addBook(title, author) {
-  books.push({ title, author });
-  localStorage.setItem('books', JSON.stringify(books));
-  displayBooks();
-}
+const bookCollection = new BookCollection();
 
 addBtn.addEventListener('click', () => {
   const title = document.querySelector('#title');
   const author = document.querySelector('#author');
-  addBook(title.value, author.value);
+  bookCollection.addBook(title.value, author.value);
 });
 
 if (localStorage.getItem('books')) {
-  books = JSON.parse(localStorage.getItem('books'));
-  displayBooks();
+  bookCollection.books = JSON.parse(localStorage.getItem('books'));
+  bookCollection.displayBooks();
 }
